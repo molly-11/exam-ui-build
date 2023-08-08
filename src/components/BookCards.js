355b35e8008002b197ec0320ddede5cca84a3238
Edit Selmeczi <selmeczi.e@gmail.com>
2023-08-08 11:16:32 +0200

    add style according to the task

--- src/components/BookCards.js
+++ src/components/BookCards.js
@@ -1,18 +1,28 @@
-import {books} from "../data";
+import { books } from "../data";
 
 function BookCards() {
   return (
     <div className="bookCards">
-      {books.map((book, index) => (
-        <div key= {index}>
-          <div >{index}</div>
-          <div className="content">
-            <h3>{book.sub}</h3>
-            <h1>{book.title}</h1>
-            <p>{book.text}</p>
+      <>
+        {books.map((book, index) => (
+          <div className="dataWrapper">
+            <div key={index+1} className="booksData">
+              <div className="number">{index+1}</div>
+              <div className="content">
+                <h3>{book.sub}</h3>
+                <h1>{book.title}</h1>
+                <p className="bookText">{book.text}</p>
+              </div>
+            </div>
+            <button>
+              <p>read more</p>
+              <span className="material-symbols-outlined arrow">
+                arrow_forward
+              </span>
+            </button>
           </div>
-        </div>
-      ))}
+        ))}
+      </>
     </div>
   );
 }
