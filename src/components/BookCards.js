76dbcbd681624d289db22d0108e26d014742061f
Edit Selmeczi <selmeczi.e@gmail.com>
2023-08-08 12:53:07 +0200

    add onHover style to the button

--- src/components/BookCards.js
+++ src/components/BookCards.js
@@ -1,6 +1,18 @@
 import { books } from "../data";
 
 function BookCards() {
+  const changeStyle=(e)=>{
+    e.target.style.background = '#1a24fb';
+    e.target.style.color = "#f8f7f3"
+    
+   }
+   const originalStyle = (e)=>{
+     e.target.style.background = '#f8f7f3';
+     e.target.style.color = '#1a24fb';
+    
+   }
+
+
   return (
     <div className="bookCards">
       <>
@@ -14,7 +26,7 @@ function BookCards() {
                 <p className="bookText">{book.text}</p>
               </div>
             </div>
-            <button>
+            <button onMouseEnter={(e)=>{changeStyle(e)}} onMouseLeave={(e)=>{originalStyle(e)}}>
               <p>read more</p>
               <span className="material-symbols-outlined arrow">
                 arrow_forward
